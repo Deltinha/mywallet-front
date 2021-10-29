55be734db49834f835f5d69c39d496d40854b5ae
Bruno <mguimaraesbruno@gmail.com>
2021-10-29 17:58:27 -0300

    Code Review: Brunão

--- src/components/MyWalletLogo/index.js
+++
@@ -1,7 +0,0 @@
-import { MyWalletStyled } from "./style";
-
-export default function MyWalletLogo(){
-    return (
-        <MyWalletStyled>MyWallet</MyWalletStyled>
-    );
-}
--- src/components/MyWalletLogo/style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const MyWalletStyled = styled.span`
-    font-size: 32px;
-    color: white;
-    font-family: 'Saira Stencil One', cursive;
-    margin-bottom: 28px;
-`;
