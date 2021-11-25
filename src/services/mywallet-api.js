97fe129e7574c580b3fd0b200f273a1e236c544f
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-25 17:07:47 -0300

    created link to deployed api

--- src/services/mywallet-api.js
+++ src/services/mywallet-api.js
@@ -1,36 +1,39 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000"
+    : "https://mywallet-deltinha.herokuapp.com/";
 
 function createHeaders(token) {
-	const config = { headers: { Authorization: `Bearer ${token}` } };
-	return config;
+  const config = { headers: { Authorization: `Bearer ${token}` } };
+  return config;
 }
 
 export function postSignUp(body) {
-	const promise = axios.post(`${BASE_URL}/sign-up`, body);
-	return promise;
+  const promise = axios.post(`${BASE_URL}/sign-up`, body);
+  return promise;
 }
 
 export function postLogIn(body) {
-	const promise = axios.post(`${BASE_URL}/login`, body);
-	return promise;
+  const promise = axios.post(`${BASE_URL}/login`, body);
+  return promise;
 }
 
-export function postEntry({body, token}) {
-	const config = createHeaders(token);
-	const promise = axios.post(`${BASE_URL}/entries`, body, config);
-	return promise;
+export function postEntry({ body, token }) {
+  const config = createHeaders(token);
+  const promise = axios.post(`${BASE_URL}/entries`, body, config);
+  return promise;
 }
 
 export function getEntries(token) {
-	const config = createHeaders(token);
-	const promise = axios.get(`${BASE_URL}/entries`, config);
-	return promise;
+  const config = createHeaders(token);
+  const promise = axios.get(`${BASE_URL}/entries`, config);
+  return promise;
 }
 
 export function postLogout(token) {
-	const config = createHeaders(token);
-	const promise = axios.post(`${BASE_URL}/logout`, {}, config);
-	return promise;
+  const config = createHeaders(token);
+  const promise = axios.post(`${BASE_URL}/logout`, {}, config);
+  return promise;
 }
