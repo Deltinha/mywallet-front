425e6e37006bfde323141d644476e077d4179ea4
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-27 10:53:00 -0300

    changed favicon and changed text of a button

--- src/screens/AddEntry/index.js
+++ src/screens/AddEntry/index.js
@@ -9,17 +9,20 @@ import { AddEntryStyled } from "./style";
 export default function AddEntry ({userData}){
     const location = useLocation().pathname;
     const history = useHistory();
-    const [header, setHeader] = useState('')
+    const [header, setHeader] = useState('');
+    const [buttonText, setButtonText] = useState('');
     const [multiplier, setMultiplier] = useState(1);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const {token} = userData;
     useEffect(()=>{
         if (location === '/add-expense') {
             setMultiplier(-1);
-            setHeader('Nova saída')
+            setHeader('Nova saída');
+            setButtonText('Salvar saída');
         } else {
             setMultiplier(1);
-            setHeader('Nova entrada')
+            setHeader('Nova entrada');
+            setButtonText('Salvar entrada');
         }
     },[location])
 
@@ -61,7 +64,7 @@ export default function AddEntry ({userData}){
             <FormStyled onSubmit={(e)=>submitEntry(e)}>
                 <TextInput required type='number' step='0.01' placeholder='Valor' onChange={(e)=>(setValue(e.target.value))}/>
                 <TextInput required type='text' placeholder='Descrição' onChange={(e)=>(setDescription(e.target.value))}/>
-                <SubmitButton type='submit' value='Salvar entrada'/>
+                <SubmitButton type='submit' value={buttonText}/>
             </FormStyled>
         </AddEntryStyled>
     );
